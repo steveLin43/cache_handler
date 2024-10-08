225394f3d961d07074427d27a6efd01b4de93be1
ericlin <ericlin@webapiz.com>
2024-10-08 17:15:39 +0800

    處理程序內快取

--- cache.go
+++ cache.go
@@ -1,5 +1,10 @@
 package cache
 
+import (
+	"log"
+	"sync"
+)
+
 // Cache 快取介面
 type Cache interface {
 	Set(key string, value interface{})
@@ -7,4 +12,57 @@ type Cache interface {
 	Del(key string)
 	DelOldest()
 	Len() int
+}
+
+// DefaultMaxBytes，即預設允許占用的最大記憶體
+const DefaultMaxBytes = 1 << 29
+
+// safeCache，即平行處理安全快取
+type safeCache struct {
+	m     sync.RWMutex
+	cache Cache
+
+	nhit, nget int
+}
+
+func newSafeCache(cache Cache) *safeCache {
+	return &safeCache{
+		cache: cache,
+	}
+}
+
+func (sc *safeCache) set(key string, value interface{}) {
+	sc.m.Lock()
+	defer sc.m.Unlock()
+	sc.cache.Set(key, value)
+}
+
+func (sc *safeCache) get(key string) interface{} {
+	sc.m.RLock()
+	defer sc.m.RUnlock()
+	sc.nget++
+	if sc.cache == nil {
+		return nil
+	}
+
+	v := sc.cache.Get(key)
+	if v != nil {
+		log.Println("[TourCache] hit")
+		sc.nhit++
+	}
+
+	return v
+}
+
+func (sc *safeCache) stat() *Stat {
+	sc.m.RLock()
+	defer sc.m.RUnlock()
+	return &Stat{
+		NHit: sc.nhit,
+		NGet: sc.nget,
+	}
+}
+
+type Stat struct {
+	NHit, NGet int // 快取命中次數, 快取取得次數
 }
